a466c7111462304607b6f5a7b76f16420b034c12
Philip Dorrell <philip@thinkinghard.com>
2013-09-01 19:36:29 +1200

    note creation, show, deletion

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -1,2 +1,33 @@
 class NotesController < ApplicationController
+  def new
+    @note = Note.new
+    @note.user = current_user
+  end
+  
+  def show
+    @note = Note.find(params[:id])
+  end
+  
+  def create
+    creation_params = note_params
+    puts "creation_params = #{creation_params.inspect}"
+    creation_params[:user_id] = current_user.id
+    note = Note.create(creation_params)
+    if note
+      redirect_to note
+    end
+  end
+  
+  def destroy
+    @note = Note.find(params[:id])
+    if @note.destroy
+      flash.notice("Note #{@note.title.inspect} was deleted");
+      redirect_to root_path
+    end
+  end
+    
+  
+  def note_params
+    params.require(:note).permit(:title, :text)
+  end
 end
